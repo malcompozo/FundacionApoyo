5a17a3f0bb8a3d6e996b7e8f693283659853d525
Malcom Pozo <malcompozo@gmail.com>
2021-06-13 19:25:31 -0400

    fusion del fron con el back en django

--- FundacionApoyo/core/views.py
+++ FundacionApoyo/core/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
 
 # llamada al fichero
-def home(request):
+def index(request):
     return render(request, "core/index.html")
 
     # llamada al fichero
-def home(request):
+def about(request):
     return render(request, "core/about.html")
 
     # llamada al fichero
-def home(request):
+def blog(request):
     return render(request, "core/blog.html")
 
     # llamada al fichero
-def home(request):
+def contact(request):
     return render(request, "core/contact.html")
 
     # llamada al fichero
-def home(request):
+def services(request):
     return render(request, "core/services.html")
 
     # llamada al fichero
-def home(request):
-    return render(request, "core/store.html")
+def visit_us(request):
+    return render(request, "core/visit_us.html")
