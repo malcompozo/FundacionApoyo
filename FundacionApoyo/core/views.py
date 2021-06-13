5f6d7c4347cf42ebc3ba4f8c7eb96647be554370
Malcom Pozo <malcompozo@gmail.com>
2021-06-12 23:12:39 -0400

    iniciando migracion django

---
+++ FundacionApoyo/core/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# llamada al fichero
+def home(request):
+    return render(request, "core/index.html")
+
+    # llamada al fichero
+def home(request):
+    return render(request, "core/about.html")
+
+    # llamada al fichero
+def home(request):
+    return render(request, "core/blog.html")
+
+    # llamada al fichero
+def home(request):
+    return render(request, "core/contact.html")
+
+    # llamada al fichero
+def home(request):
+    return render(request, "core/services.html")
+
+    # llamada al fichero
+def home(request):
+    return render(request, "core/store.html")
