6c20a60b3996ae060d5dc0b63009cfe48aa39446
Malcom Pozo <malcompozo@gmail.com>
2021-06-16 01:28:14 -0400

    template contacto con envio de email automatico

--- FundacionApoyo/FundacionApoyo/urls.py
+++ FundacionApoyo/FundacionApoyo/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,6 +23,8 @@ urlpatterns = [
     path('',include('core.urls')),
     #path de services
     path('services/', include('services.urls')),
+    #path de contacto
+    path('contact/',include('contact.urls')),
     #path de admin
     path('admin/', admin.site.urls),
 ]
--- FundacionApoyo/contact/urls.py
+++ FundacionApoyo/contact/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    path('', views.services, name="services"),
-
+    path('', views.contact, name="contact"),
 ]
